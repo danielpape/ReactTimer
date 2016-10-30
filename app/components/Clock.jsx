61d22ee0f35c23586c309ae5f3ca8b4fdba7402d
Daniel Pape <danielwpape@gmail.com>
2016-10-30 17:04:02 +0000

    Added Navigation

---
+++ app/components/Clock.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Clock = () => {
+  return(
+    <div>
+      <p>Clock</p>
+    </div>
+  );
+};
+
+module.exports = Clock;
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -7,8 +7,8 @@ var Nav = (props) => {
       <div className="top-bar-left">
         <ul className="menu">
           <li className="menu-text">React Timer App</li>
-          <li><Link to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Timer</Link></li>
-          <li><Link to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Countdown</Link></li>
+          <li><Link to="/timer" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Timer</Link></li>
+          <li><Link to="/countdown" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Countdown</Link></li>
         </ul>
       </div>
       <div className="top-bar-right">
