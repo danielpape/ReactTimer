421db9040d7ba9fb92bff604f517fd5532003375
Daniel Pape <danielwpape@gmail.com>
2016-11-28 21:43:07 +0000

    Added layout for timer

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,35 @@
 var React = require('react');
+var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
-var Timer = () => {
-  return(
-    <div>
-      <p>Timer</p>
-    </div>
-  );
-};
+var Timer = React.createClass({
+    getInitialState: function() {
+        return ({count: 0, countdownStatus: 'stopped'})
+    },
+    handleStatusChange: function(){
+      this.setState=({countdownStatus:newStatus})
+    },
+    handleSetCountdown: function(){
+      this.setState=({countdownStatus:'started',count:seconds})
+    },
+    render: function() {
+        var {countdownStatus, count} = this.state;
+        var renderControlArea = () => {
+            if (countdownStatus !== 'stopped') {
+                return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+            } else {
+                return <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+            }
+        };
+        return (
+            <div>
+                <h1 className="page-title">Timer</h1>
+                <Clock totalSeconds={count}/>
+                {renderControlArea()}
+            </div>
+        );
+    }
+});
 
 module.exports = Timer;
